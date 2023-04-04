bd81575f44f6576cae46fb8151f07614ab350343
kunal-gitt <kunalthakur2023@gmail.com>
2023-04-04 15:56:12 +0530

    Project created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Components/Layout/Header";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
