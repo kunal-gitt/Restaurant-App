ceef08ff1e31ee9aaca08064a58ef6ffd271129c
kunal-gitt <kunalthakur2023@gmail.com>
2023-04-05 12:53:18 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from "./Components/Layout/Header";
 
 import "./App.css";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <main>
+        <Meals />
+      </main>
     </div>
   );
 }
