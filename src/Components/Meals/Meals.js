45dc1f5dd041bbce181d131fa0b1ad0e84182f94
kunal-gitt <kunalthakur2023@gmail.com>
2023-04-05 11:59:02 +0530

    Deliverables Restaurant summary

---
+++ src/Components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import MealsSummary from "./MealsSummary";
+
+import Availablemeals from "./AvailableMeals";
+import { Fragment } from "react";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+    </Fragment>
+  );
+};
+
+export default Meals;
